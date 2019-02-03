b2d47cfa5bc5cef0c8d60189f4b63acac1c5677d
Alden <aj.molina092@gmail.com>
2019-02-02 19:11:28 -0500

    Started a cookie order class and a master order class

---
+++ CookieOrders/CookieOrder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CookieOrders
+{
+    class CookieOrder
+    {
+        public string Variety { get; set; }
+        public int NumberOfBoxes { get; set; }
+
+        public CookieOrder(string variety, int numberOfBoxes)
+        {
+            Variety = variety;
+            NumberOfBoxes = numberOfBoxes;
+        }
+    }
+}
---
+++ CookieOrders/MasterOrder.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CookieOrders
+{
+    class MasterOrder
+    {
+        public List<CookieOrder> orders { get; set; }
+
+        public void MasterOrder()
+        {
+            
+        }
+
+        public void AddOrder(CookieOrder theOrder)
+        {
+            orders.Add(theOrder);
+        }
+
+        public int GetTotalBoxes()
+        {
+            int totalBoxes = 0;
+            foreach (CookieOrder box in orders)
+            {
+                totalBoxes += box.NumberOfBoxes;
+            }
+            return totalBoxes;
+        }
+
+        public void RemoveVariety(string variety)
+        {
+            foreach (CookieOrder box in orders)
+            {
+                if (box.Variety == variety)
+                {
+                    orders.Remove(box);
+                }
+            }
+        }
+
+        public int GetVarietyBoxes(string variety)
+        {
+            int totalBoxesOfVariety = 0;
+            foreach (CookieOrder box in orders)
+            {
+                if (box.Variety == variety)
+                {
+                    totalBoxesOfVariety += box.NumberOfBoxes;
+                }
+            }
+            return totalBoxesOfVariety;
+        }
+
+        public void ShowOrder()
+        {
+            int orderNumber = 0;
+            foreach (CookieOrder box in orders)
+            {
+                Console.WriteLine("Order Number " + (orderNumber + 1) + " Variety: " + box.Variety + " Number of Boxes Ordered: " + box.NumberOfBoxes);
+            }
+        }
+    }
+
+    
+
+}
